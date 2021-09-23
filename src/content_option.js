985137c277c1ea9bef7111ea9e065e61f1405d1c
Temitope <ojotemitopeidnoble10@gmail.com>
2021-09-23 14:34:12 -0700

    aaa

---
+++ src/content_option.js
@@ -0,0 +1,205 @@
+const logotext = "Temitope Joshua";
+const meta = {
+  title: "Temitope J",
+  description:
+    "I’m Temitope Joshua, I am a front end developer base in Ogun state Nigeria",
+};
+
+const introdata = {
+  title: "I’m Temitope Joshua",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "I am an experienced front end developer, I have more than two years in Javascript and Javascript frameworks and libraries, I am a diligent and hardworking person and my inner power is my flexibility to adapt and ready to learn and relearn to provide ultimate performance ",
+
+  your_img_url:
+    "https://miro.medium.com/max/10884/1*5TRuG7tG0KrZJXKoFtHlSg.jpeg",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [
+  {
+    jobtitle: "WEB DESIGN",
+    where: "CEPHAS",
+    date: "2020",
+  },
+
+  
+];
+
+const skills = [
+  {
+    name: "HTML",
+    value: 100,
+  },
+  {
+    name: "CSS",
+    value: 70,
+  },
+  {
+    name: "BOOTSTRAP",
+    value: 80,
+  },
+  {
+    name: "jAVASCRIPTS",
+    value: 70,
+  },
+  {
+    name: "REACT",
+    value: 70,
+  },
+  {
+    name: "REACT-REDUX",
+    value: 70,
+  },
+  {
+    name: "NEXT JS",
+    value: 50,
+  },
+  {
+    name: "REACT NATIVE",
+    value: 70,
+  },
+  {
+    name: "VUE JS",
+    value: 50,
+  },
+  {
+    name: "NUXT JS",
+    value: 50,
+  },
+  {
+    name: "VUEX",
+    value: 60,
+  },
+  {
+    name: "VUETIFLY",
+    value: 50,
+  },
+];
+
+const services = [
+  {
+    title: "WEB DESIGN AND DEVELOPMENT",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "ojotemitopeidnoble10@gmail.com",
+  YOUR_FONE: "(+234) 8132081560",
+  description:
+    "I am an experienced front end developer, I have two plus in Javascrpt and Javascript frameworks and libraries, I am a diligent and hardworking person and my inner power is my flexibility to adapt and ready to earn and relearn to provide ultimate performance ",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
